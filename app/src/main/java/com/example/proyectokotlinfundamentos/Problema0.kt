c2f07872070fe750028ca532502417f562d90451
Fredy Laverde <fredys.laverdem@uatdeo.edu.co>
2024-08-07 16:03:55 -0500

    Problema 0

--- app/src/main/java/com/example/proyectokotlinfundamentos/Problema0.kt
+++ app/src/main/java/com/example/proyectokotlinfundamentos/Problema0.kt
@@ -1,7 +1,7 @@
 package com.example.proyectokotlinfundamentos
 
-// Nombre:
-// Fecha:
+// Nombre: Fredy Santiago Laverde Mora
+// Fecha: 08/08/2024
 // Descripción: Solución del Problema 0 de la guía de Kotlin fundamentos
 
 // Función principal
@@ -12,5 +12,17 @@ fun main() {
 // Función que debe desarrollar la lógica para la solución del problema
 fun problema0() {
     // Desarrolle aquí la lógica
+    println("    |\\/\\  ,.\n" +
+            "    /   `' |,-,\n" +
+            "   /         /_\n" +
+            " _/            /\n" +
+            "(.-,--.       /\n" +
+            "/o/  o \\     /\n" +
+            "\\_\\    /   _/\n" +
+            "(__`--'    _)\n" +
+            " /         |\n" +
+            "(_____,'    \\  \n" +
+            "   \\_       _\\\n" +
+            "     `._..-'")
 
 }
